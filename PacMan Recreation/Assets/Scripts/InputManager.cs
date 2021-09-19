35854e3df03e802130f0ba32257300c08a27ea90
pricilla-tran <71370147+pricilla-tran@users.noreply.github.com>
2021-09-19 21:57:57 +1000

    PacStudent Movement Done!
    PacStudent goes clockwise around the inner block!

--- PacMan Recreation/Assets/Scripts/InputManager.cs
+++ PacMan Recreation/Assets/Scripts/InputManager.cs
@@ -18,24 +18,50 @@ public class InputManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // (-20.5, 12.5, 0)
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
             animatorController.SetTrigger("UpParam");
+            CreateTween(new Vector3(-20.5f, 12.5f, 0), 1.5f);
         }
 
+        // (-15.5, 8.5, 0)
         else if (Input.GetKeyDown(KeyCode.DownArrow))
         {
             animatorController.SetTrigger("DownParam");
+            CreateTween(new Vector3(-15.5f, 8.5f, 0), 1.5f);
         }
 
+        // (-20.5, 8.5, 0)
         else if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
             animatorController.SetTrigger("LeftParam");
+            CreateTween(new Vector3(-20.5f, 8.5f, 0), 1.5f);
         }
 
+        // (-15.5, 12.5, 0)
         else if (Input.GetKeyDown(KeyCode.RightArrow))
         {
             animatorController.SetTrigger("RightParam");
+            CreateTween(new Vector3(-15.5f, 12.5f, 0), 1.5f);
         }
+    }
+
+    public void CreateTween(Vector3 endPosition, float duration)
+    {
+        //for (int i = 0; i < itemList.Count; i++)
+        //{
+            bool addedTween = tweener.AddTween(character.transform, character.transform.position, endPosition, duration);
+            /*
+            if (addedTween)
+            {
+                break;
+            }
+            else if (!addedTween)
+            {
+
+            }
+            */
+        //}
     }
 }
--- PacMan Recreation/Assets/Scripts/Tweener.cs
+++ PacMan Recreation/Assets/Scripts/Tweener.cs
@@ -4,25 +4,72 @@ using UnityEngine;
 
 public class Tweener : MonoBehaviour
 {
-    private Tween activeTween;
+    private List<Tween> activeTweens;
+    float distance;
+    float timeFraction;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        activeTweens = new List<Tween>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
-
-    public void AddTween(Transform targetObject, Vector3 startPos, Vector3 endPos, float duration)
-    {
-        if (activeTween == null)
+        if (activeTweens != null)
         {
-            activeTween = new Tween(targetObject, startPos, endPos, Time.time, duration);
+            for (int i = 0; i < activeTweens.Count; i++)
+            {
+                distance = Vector3.Distance(activeTweens[i].Target.position, activeTweens[i].EndPos);
+                // Linear Interpolation 
+                timeFraction = (Time.time - activeTweens[i].StartTime) / activeTweens[i].Duration;
+                // Cubic Ease In Interpolation
+                //timeFraction = timeFraction * timeFraction * timeFraction;
+
+                if (distance > 0.1f)
+                {
+                    activeTweens[i].Target.position = Vector3.Lerp(activeTweens[i].StartPos, activeTweens[i].EndPos, timeFraction);
+                }
+                else
+                {
+                    activeTweens[i].Target.position = activeTweens[i].EndPos;
+                    activeTweens.Remove(activeTweens[i]);
+                }
+            }
         }
     }
+
+    public bool AddTween(Transform targetObject, Vector3 startPos, Vector3 endPos, float duration)
+    {
+        if (activeTweens == null)
+        {
+            activeTweens.Add(new Tween(targetObject, startPos, endPos, Time.time, duration));
+        }
+
+        if (!TweenExists(targetObject))
+        {
+            //Tween newTween = new Tween(activeTweens[i].StartPos,);
+            activeTweens.Add(new Tween(targetObject, startPos, endPos, Time.time, duration));
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+
+    }
+
+    public bool TweenExists(Transform target)
+    {
+        foreach (Tween tween in activeTweens)
+        {
+            if (tween.Target.transform == target)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
