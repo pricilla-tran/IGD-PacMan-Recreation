481e8afbb9810257cd9570f82c119695b4a060d9
pricilla-tran <71370147+pricilla-tran@users.noreply.github.com>
2021-09-21 18:47:05 +1000

    Added Walking Sound
    Make sure to merge to Feature-Audio Later!

--- PacMan Recreation/Assets/Scripts/SoundPlayer.cs
+++ PacMan Recreation/Assets/Scripts/SoundPlayer.cs
@@ -15,10 +15,31 @@ public class SoundPlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-      if (Input.GetKeyDown(KeyCode.LeftArrow))
-      {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
             walkingSound.Play();
-      }
-      
+            Invoke("StopSound", 1.5f);
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            walkingSound.Play();
+            Invoke("StopSound", 1.5f);
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            walkingSound.Play();
+            Invoke("StopSound", 1.5f);
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            walkingSound.Play();
+            Invoke("StopSound", 1.5f);
+        }
+
+    }
+
+    void StopSound()
+    {
+        walkingSound.Stop();
     }
 }
