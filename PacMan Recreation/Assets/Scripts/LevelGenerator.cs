77813e1c1d0b0cbafa32406b65271b4b0cc217ce
pricilla-tran <71370147+pricilla-tran@users.noreply.github.com>
2021-09-21 16:32:57 +1000

    Initialised Sprites in Level Gen Script
    Having some unity error that pops up which is kind of stressing me out

--- PacMan Recreation/Assets/Scripts/LevelGenerator.cs
+++ PacMan Recreation/Assets/Scripts/LevelGenerator.cs
@@ -2,8 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class LevelGenerator : MonoBehaviour
 {
+    public GameObject pacStudent;
+    public GameObject Ghost1;
+    public GameObject Ghost2;
+    public GameObject Ghost3;
+    public GameObject Ghost4;
+
     int[,] levelMap = {
         {1,2,2,2,2,2,2,2,2,2,2,2,2,7},
         {2,5,5,5,5,5,5,5,5,5,5,5,5,4},
@@ -23,9 +29,14 @@ public class NewBehaviourScript : MonoBehaviour
     };
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        pacStudent.transform.position = new Vector3(-20.5f, 12.5f, 0);
+        // Testing initial positions
+        Ghost1.transform.position = new Vector3(-7.5f, -0.5f, 0); // Ghost 2 Position
+        Ghost2.transform.position = new Vector3(-8.5f, -1.5f, 0); // Ghost 3 Position
+        Ghost3.transform.position = new Vector3(-7.5f, -1.5f, 0); // Ghost 4 Position
+        Ghost4.transform.position = new Vector3(-8.5f, -0.5f, 0); // Ghost 1 Position
     }
 
     // Update is called once per frame
