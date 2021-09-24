f08ca6223169595e9916a59e4619a8cf56391541
pricilla-tran <71370147+pricilla-tran@users.noreply.github.com>
2021-09-25 01:10:18 +1000

    Finished Right Quadrant!
    Yayyy you go girl! Now we can pump out the bottom two tomorrow!

--- PacMan Recreation/Assets/Scripts/LevelGenLayout.cs
+++ PacMan Recreation/Assets/Scripts/LevelGenLayout.cs
@@ -4,12 +4,25 @@ using UnityEngine;
 
 public class LevelGenLayout : MonoBehaviour
 {
-    public GameObject LevelManager;
+    public GameObject LevelManager; // Parent Object 
+    private GameObject newQuadarant;
+    public List<Transform> initialPositions;
     // Start is called before the first frame update
     void Start()
     {
         //Instantiate(LevelManager, new Vector3(-13.5f, 14.5f, 0), Quaternion.identity);
         //Instantiate(LevelManager, new Vector3(0.5f, 14.5f, 0f), Quaternion.Euler(0, 180, 45));
+        //newQuadarant.transform.parent = LevelManager.transform;
+        List<Transform> initialPositions = new List<Transform>();
+
+        GameObject quad_1 = Instantiate(LevelManager, new Vector3(-13.5f, 14.5f, 0), Quaternion.identity);
+        GameObject quad_2 = Instantiate(LevelManager, new Vector3(13.5f, 14.5f, 0f), Quaternion.Euler(0, 0, 45));
+
+        //initialPositions.Add(quad_1.transform);
+        //initialPositions.Add(quad_2.transform);
+
+
+
     }
 
     // Update is called once per frame
--- PacMan Recreation/Assets/Scripts/LevelGenerator.cs
+++ PacMan Recreation/Assets/Scripts/LevelGenerator.cs
@@ -4,14 +4,15 @@ using UnityEngine;
 
 public class LevelGenerator : MonoBehaviour
 {
-    public GameObject pacStudent;
-    public GameObject Ghost1;
-    public GameObject Ghost2;
-    public GameObject Ghost3;
-    public GameObject Ghost4;
+    //public GameObject pacStudent;
+    //public GameObject Ghost1;
+    //public GameObject Ghost2;
+    //public GameObject Ghost3;
+    //public GameObject Ghost4;
     //public GameObject LevelManager;
     //public GameObject PowerPellet;
     private bool powerPelletAdded = false;
+    private int PPCount = 0;
     private Animator anim;
     public GameObject[] levelElements;
     private Vector3 position;
@@ -20,34 +21,60 @@ public class LevelGenerator : MonoBehaviour
     private int elementUp;
     private int elementDown;
 
-
+    
     int[,] levelMap = {
-        {1,2,2,2,2,2,2,2,2,2,2,2,2,7},
-        {2,5,5,5,5,5,5,5,5,5,5,5,5,4},
-        {2,5,3,4,4,3,5,3,4,4,4,3,5,4},
-        {2,6,4,0,0,4,5,4,0,0,0,4,5,4},
-        {2,5,3,4,4,3,5,3,4,4,4,3,5,3},
-        {2,5,5,5,5,5,5,5,5,5,5,5,5,5},
-        {2,5,3,4,4,3,5,3,3,5,3,4,4,4},
-        {2,5,3,4,4,3,5,4,4,5,3,4,4,3},
-        {2,5,5,5,5,5,5,4,4,5,5,5,5,4},
-        {1,2,2,2,2,1,5,4,3,4,4,3,0,4},
-        {0,0,0,0,0,2,5,4,3,4,4,3,0,3},
-        {0,0,0,0,0,2,5,4,4,0,0,0,0,0},
-        {0,0,0,0,0,2,5,4,4,0,3,4,4,0},
-        {2,2,2,2,2,1,5,3,3,0,4,0,0,0},
-        {0,0,0,0,0,0,5,0,0,0,4,0,0,0},
+        // Top-Left and Right
+        {1,2,2,2,2,2,2,2,2,2,2,2,2,7, 7,2,2,2,2,2,2,2,2,2,2,2,2,1},
+        {2,5,5,5,5,5,5,5,5,5,5,5,5,4, 4,5,5,5,5,5,5,5,5,5,5,5,5,2},
+        {2,5,3,4,4,3,5,3,4,4,4,3,5,4, 4,5,3,4,4,4,3,5,3,4,4,3,5,2},
+        {2,6,4,0,0,4,5,4,0,0,0,4,5,4, 4,5,4,0,0,0,4,5,4,0,0,4,6,2},
+        {2,5,3,4,4,3,5,3,4,4,4,3,5,3, 3,5,3,4,4,4,3,5,3,4,4,3,5,2},
+        {2,5,5,5,5,5,5,5,5,5,5,5,5,5, 5,5,5,5,5,5,5,5,5,5,5,5,5,2},
+        {2,5,3,4,4,3,5,3,3,5,3,4,4,4, 4,4,4,3,5,3,3,5,3,4,4,3,5,2},
+        {2,5,3,4,4,3,5,4,4,5,3,4,4,3, 3,4,4,3,5,4,4,5,3,4,4,3,5,2},
+        {2,5,5,5,5,5,5,4,4,5,5,5,5,4, 4,5,5,5,5,4,4,5,5,5,5,5,5,2},
+        {1,2,2,2,2,1,5,4,3,4,4,3,0,4, 4,0,3,4,4,3,4,5,1,2,2,2,2,1},
+        {0,0,0,0,0,2,5,4,3,4,4,3,0,3, 3,0,3,4,4,3,4,5,2,0,0,0,0,0},
+        {0,0,0,0,0,2,5,4,4,0,0,0,0,0, 0,0,0,0,0,4,4,5,2,0,0,0,0,0},
+        {0,0,0,0,0,2,5,4,4,0,3,4,4,0, 0,4,4,3,0,4,4,5,2,0,0,0,0,0},
+        {2,2,2,2,2,1,5,3,3,0,4,0,0,0, 0,0,0,4,0,3,3,5,1,2,2,2,2,2},
+        {0,0,0,0,0,0,5,0,0,0,4,0,0,0, 0,0,0,4,0,0,0,5,0,0,0,0,0,0},
+        // Bottom-Left 
+        //{0,0,0,0,0,0,5,0,0,0,4,0,0,0, 0},
+        //{2,2,2,2,2,1,5,3,3,0,4,0,0,0, 0},
+        //{0,0,0,0,0,2,5,4,4,0,3,4,4,0, 0},
     };
+    
+    /*
+    int[,] levelMap =
+     {
+     {1,2,2,2,2,2,2,2,2,2,2,2,2,7},
+     {2,5,5,5,5,5,5,5,5,5,5,5,5,4},
+     {2,5,3,4,4,3,5,3,4,4,4,3,5,4},
+     {2,6,4,0,0,4,5,4,0,0,0,4,5,4},
+     {2,5,3,4,4,3,5,3,4,4,4,3,5,3},
+     {2,5,5,5,5,5,5,5,5,5,5,5,5,5},
+     {2,5,3,4,4,3,5,3,3,5,3,4,4,4},
+     {2,5,3,4,4,3,5,4,4,5,3,4,4,3},
+     {2,5,5,5,5,5,5,4,4,5,5,5,5,4},
+     {1,2,2,2,2,1,5,4,3,4,4,3,0,4},
+     {0,0,0,0,0,2,5,4,3,4,4,3,0,3},
+     {0,0,0,0,0,2,5,4,4,0,0,0,0,0},
+     {0,0,0,0,0,2,5,4,4,0,3,4,4,0},
+     {2,2,2,2,2,1,5,3,3,0,4,0,0,0},
+     {0,0,0,0,0,0,5,0,0,0,4,0,0,0},
+     };
+    */
 
     // Start is called before the first frame update
     void Awake()
     {
-        pacStudent.transform.position = new Vector3(-12.5f, 13.5f, 0);
+        //pacStudent.transform.position = new Vector3(-12.5f, 13.5f, 0);
         // Testing initial positions
-        Ghost1.transform.position = new Vector3(0.5f, 0.5f, 0); // Ghost 2 Original Position
-        Ghost2.transform.position = new Vector3(-0.5f, -0.5f, 0); // Ghost 3 Original Position
-        Ghost3.transform.position = new Vector3(0.5f, -0.5f, 0); // Ghost 4 Original Position
-        Ghost4.transform.position = new Vector3(-0.5f, 0.5f, 0); // Ghost 1 Original Position
+        //Ghost1.transform.position = new Vector3(0.5f, 0.5f, 0); // Ghost 2 Original Position
+        //Ghost2.transform.position = new Vector3(-0.5f, -0.5f, 0); // Ghost 3 Original Position
+        //Ghost3.transform.position = new Vector3(0.5f, -0.5f, 0); // Ghost 4 Original Position
+        //Ghost4.transform.position = new Vector3(-0.5f, 0.5f, 0); // Ghost 1 Original Position
         //Instantiate(levelElements[5], new Vector3(-13.5f + 1, 14.5f - 2, 0), Quaternion.identity);
         //anim = levelElements[5].GetComponent<Animator>();
         //anim = levelElements[5].GetComponent<Animator>();
@@ -59,6 +86,7 @@ public class LevelGenerator : MonoBehaviour
     private void Start()
     {
         //Instantiate(levelElements[5], new Vector3(-13.5f + 1, 14.5f - 3, 0), Quaternion.identity);
+
     }
 
     // Update is called once per frame
@@ -72,6 +100,7 @@ public class LevelGenerator : MonoBehaviour
 
                 //while (i >= 0 && j >= 0)
                 //{
+                
                 if (i >= 0 && j >= 0)
                 {
 
@@ -82,19 +111,38 @@ public class LevelGenerator : MonoBehaviour
                             break;
 
                         case 1: // Outside Corner
-                            if (elementLeft == 2 && i > 0 && levelMap[i-1, j] == 2) 
+                            if (elementLeft == 2 && i > 0 && levelMap[i - 1, j] == 2) // OC 4 and OC 2 (but need to make a second one for that) 
+                            {
+                                if (j < 14 && i < 10)
+                                {
+                                    Instantiate(levelElements[0], new Vector3(-13.5f + j, 14.5f - i, 0), Quaternion.Euler(0, 0, 90));
+                                    elementLeft = levelMap[i, j];
+                                }
+                                else
+                                {
+                                    Instantiate(levelElements[0], new Vector3(-13.5f + j, 14.5f - i, 0), Quaternion.Euler(0, 0, 180));
+                                    elementLeft = levelMap[i, j];
+                                }
+                            }
+                            else if (elementLeft == 2 && i > 0 && levelMap[i-1, j] == 2) // OC 4 and OC 2 (but need to make a second one for that) 
                             {
                                 Instantiate(levelElements[0], new Vector3(-13.5f + j, 14.5f - i, 0), Quaternion.Euler(0, 0, 180));
                                 elementLeft = levelMap[i, j];
                             }
-                            else if (i > 0 && levelMap[i - 1, j] == 2)
+                            
+                            else if (i > 0 && levelMap[i - 1, j] == 2 && levelMap[i+1,j] == 0) // OC 4 (on the right) 
                             {
                                 Instantiate(levelElements[0], new Vector3(-13.5f + j, 14.5f - i, 0), Quaternion.Euler(0, 0, 90));
                                 elementLeft = levelMap[i, j];
                             }
-                            else if (elementLeft == 2 && levelMap[i, j + 1] == 5)
+                            else if (i > 0 && elementLeft == 2 && levelMap[i-1, j] == 5 && levelMap[i+1,j] == 2) // OC 3
                             {
                                 Instantiate(levelElements[0], new Vector3(-13.5f + j, 14.5f - i, 0), Quaternion.Euler(0, 0, 270));
+                                elementLeft = levelMap[i, j];
+                            }
+                            else if (j > 14 && elementLeft == 2) // End Corner Row 0 
+                            {
+                                Instantiate(levelElements[0], new Vector3(-13.5f + j, 14.5f - i, 0), Quaternion.Euler(0, 180, 0));
                                 elementLeft = levelMap[i, j];
                             }
                             else
@@ -131,30 +179,53 @@ public class LevelGenerator : MonoBehaviour
                             
                             else if (elementLeft == 4 && levelMap[i + 1, j] == 4 && levelMap[i - 1, j] == 3) // for bottom corner of T section 
                             {
-                                Instantiate(levelElements[2], new Vector3(-13.5f + j, 14.5f - i, 0), Quaternion.Euler(0, 0, 0));
-                                elementLeft = levelMap[i, j];
+                                if (j > 14)
+                                {
+                                    Instantiate(levelElements[2], new Vector3(-13.5f + j, 14.5f - i, 0), Quaternion.Euler(0, 0, 270));
+                                    elementLeft = levelMap[i, j];
+                                }
+                                else
+                                {
+                                    Instantiate(levelElements[2], new Vector3(-13.5f + j, 14.5f - i, 0), Quaternion.Euler(0, 0, 0));
+                                    elementLeft = levelMap[i, j];
+                                }
                             }
                             else if (elementLeft == 4 && levelMap[i + 1, j] == 4 && levelMap[i - 1, j] == 4) // for bottom corner of T section 
                             {
                                 Instantiate(levelElements[2], new Vector3(-13.5f + j, 14.5f - i, 0), Quaternion.Euler(0, 180, 0));
                                 elementLeft = levelMap[i, j];
                             }
+                            else if (elementLeft == 3 && levelMap[i - 1, j] == 4 && levelMap[i+1,j] == 4)
+                            {
+                                Instantiate(levelElements[2], new Vector3(-13.5f + j, 14.5f - i, 0), Quaternion.Euler(0, 180, 270));
+                                elementLeft = levelMap[i, j];
+                            }
+                            
                             else if (elementLeft == 3 && levelMap[i-1, j] == 4)
                             {
                                 Instantiate(levelElements[2], new Vector3(-13.5f + j, 14.5f - i, 0), Quaternion.Euler(0, 0, 180));
                                 elementLeft = levelMap[i, j];
                             }
-
-                            else if (elementLeft == 4 && levelMap[i - 1, j] == 4)
+                            
+                            else if (elementLeft == 4 && levelMap[i - 1, j] == 4) // Top Corner of Side T
                             {
-                                Instantiate(levelElements[2], new Vector3(-13.5f + j, 14.5f - i, 0), Quaternion.Euler(0, 0, 90)); // 90 works for T points, 180 for big blocks
-                                elementLeft = levelMap[i, j];
+                                if (j > 14 && i < 10)
+                                {
+                                    Instantiate(levelElements[2], new Vector3(-13.5f + j, 14.5f - i, 0), Quaternion.Euler(0, 0, 180)); // 90 works for T points, 180 for big blocks
+                                    elementLeft = levelMap[i, j];
+                                }
+                                else {
+                                    Instantiate(levelElements[2], new Vector3(-13.5f + j, 14.5f - i, 0), Quaternion.Euler(0, 0, 90)); // 90 works for T points, 180 for big blocks
+                                    elementLeft = levelMap[i, j];
+                                }
                             }
-                            else if (elementLeft == 4 && levelMap[i - 1, j] == 3)
+                            else if (elementLeft == 4 && levelMap[i - 1, j] == 3) // Bottom Corner of Side T
                             {
+
                                 Instantiate(levelElements[2], new Vector3(-13.5f + j, 14.5f - i, 0), Quaternion.Euler(0, 0, 180));
                                 elementLeft = levelMap[i, j];
                             }
+                            
                             else if (elementLeft == 4)
                             {
                                 Instantiate(levelElements[2], new Vector3(-13.5f + j, 14.5f - i, 0), Quaternion.Euler(0, 180, 0));
@@ -170,7 +241,7 @@ public class LevelGenerator : MonoBehaviour
                                 Instantiate(levelElements[2], new Vector3(-13.5f + j, 14.5f - i, 0), Quaternion.Euler(0, 180, 180));
                                 elementLeft = levelMap[i, j];
                             }
-                            else if (elementLeft == 3)
+                            else if (elementLeft == 3) // Connected Corners
                             {
                                 Instantiate(levelElements[2], new Vector3(-13.5f + j, 14.5f - i, 0), Quaternion.Euler(0, 180, 0));
                                 elementLeft = levelMap[i, j];
@@ -185,8 +256,16 @@ public class LevelGenerator : MonoBehaviour
                         case 4: // Inside Wall
                             if (elementLeft == 3)
                             {
-                                Instantiate(levelElements[3], new Vector3(-13.5f + j, 14.5f - i, 0), Quaternion.Euler(0, 0, 0));
-                                elementLeft = levelMap[i, j];
+                                if (j > 14 && levelMap[i-1,j] == 4 && levelMap[i+1,j] == 4 || j > 14 && levelMap[i - 1, j] == 4 && levelMap[i + 1, j] == 3)
+                                {
+                                    Instantiate(levelElements[3], new Vector3(-13.5f + j, 14.5f - i, 0), Quaternion.Euler(0, 0, 90));
+                                    elementLeft = levelMap[i, j];
+                                }
+                                else 
+                                {
+                                    Instantiate(levelElements[3], new Vector3(-13.5f + j, 14.5f - i, 0), Quaternion.Euler(0, 0, 0));
+                                    elementLeft = levelMap[i, j];
+                                }
                             }
                             else if (elementLeft == 4 && levelMap[i + 1, j] == 0 || elementLeft == 4 && levelMap[i + 1, j] == 5)
                             {
@@ -233,13 +312,21 @@ public class LevelGenerator : MonoBehaviour
                             {
                                 Instantiate(levelElements[5], new Vector3(-13.5f + j, 14.5f - i, 0), Quaternion.identity);
                                 elementLeft = levelMap[i, j];
-                                powerPelletAdded = true; 
+                                powerPelletAdded = true;
+                                PPCount++;
                             }
+                            /*else if (powerPelletAdded && PPCount > 0)
+                            {
+                                Instantiate(levelElements[5], new Vector3(-13.5f + j, 14.5f - i, 0), Quaternion.identity);
+                                elementLeft = levelMap[i, j];
+                                powerPelletAdded = false;
+                            }
+                            */
                             //
                             //anim = levelElements[5].GetComponent<Animator>();
                             //anim.enabled = true;
                             //anim.Play("PowerPellet_Flash");
-                            
+
                             break;
 
                         case 7: // T-Junction
