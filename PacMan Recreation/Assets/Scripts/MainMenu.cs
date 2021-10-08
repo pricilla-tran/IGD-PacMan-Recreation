9fa831ac085bfd99152b04459970b68f4fd9dc62
pricilla-tran <71370147+pricilla-tran@users.noreply.github.com>
2021-10-08 23:06:33 +1100

    Start Screen Changes
    Major Changes, just need an animated border for it, but I want to get all the PacMan Movement and Cherry Movement done ASAP!

--- PacMan Recreation/Assets/Scripts/MainMenu.cs
+++ PacMan Recreation/Assets/Scripts/MainMenu.cs
@@ -13,14 +13,17 @@ public class MainMenu : MonoBehaviour
     private float timer;
     private float moveWait = 1.5f;
     private float moveCycle;
+    private Tweener BGTweener;
 
     // Start is called before the first frame update
     void Start()
     {
         lvl1Button.onClick.AddListener(Level1Load);
         lvl2Button.onClick.AddListener(Level2Load);
-        pizza[0].transform.position = new Vector3(-10, 8, 0);
-        pizza[1].transform.position = new Vector3(10, -8, 0); 
+        pizza[0].transform.position = new Vector3(0, 0, 0);
+        //BGTweener = gameObject.GetComponent<Tweener>();
+        
+        //pizza[1].transform.position = new Vector3(10, -8, 0); 
     }
 
     // Update is called once per frame
@@ -31,35 +34,27 @@ public class MainMenu : MonoBehaviour
 
     void BorderMovement()
     {
+        
         timer += Time.deltaTime;
+        /*
         if ((int)timer == (int)moveWait && moveCycle == 0)
         {
-            timer = 0;
-            pizza[0].transform.position += Vector3.down * 16;
-            pizza[1].transform.position += Vector3.up * 16;
+            //timer = 0;
+            //pizza[0].transform.position += Vector3.down * 16;
+            //pizza[1].transform.position += Vector3.up * 16;
+            pizza[0].transform.position = Vector3.Lerp( pizza[0].transform.position, Vector3.right * 2, 0.5f);
             moveCycle++;
         }
         else if ((int)timer == (int)moveWait && moveCycle == 1)
         {
             timer = 0;
-            pizza[0].transform.position += Vector3.right * 20;
-            pizza[1].transform.position += Vector3.left * 20;
-            moveCycle++;
-        }
-        else if ((int)timer == (int)moveWait && moveCycle == 2)
-        {
-            timer = 0;
-            pizza[0].transform.position += Vector3.up * 16;
-            pizza[1].transform.position += Vector3.down * 16;
-            moveCycle++;
-        }
-        else if ((int)timer == (int)moveWait && moveCycle == 3)
-        {
-            timer = 0;
-            pizza[0].transform.position += Vector3.left * 20;
-            pizza[1].transform.position += Vector3.right * 20;
+            //pizza[0].transform.position += Vector3.right * 16;
+            //pizza[1].transform.position += Vector3.left * 20;
+            //pizza[0].transform.position = Vector3.Lerp(pizza[0].transform.position, Vector3.left * 2, 0.5f);
             moveCycle = 0;
         }
+        */
+
     }
 
     void Level1Load()
